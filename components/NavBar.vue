cefff4a335bb69c964674433e288421c1b57d9a6
Nicholas Wong <40312631+kozr@users.noreply.github.com>
2020-12-04 02:35:59 -0800

    Update NavBar.vue

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -12,7 +12,7 @@
           <a v-scroll-to="'#about'" href="#" class="navbar-item">About</a>
           <a v-if="faq" v-scroll-to="'#faq'" href="#" class="navbar-item">FAQ</a>
           <a v-if="sponsors" v-scroll-to="'#sponza'" href="#" class="navbar-item">Sponsors</a>
-          <a href="https://2020.nwhacks.io/" rel="noopener" target="_blank" class="navbar-item">2019</a>
+          <a href="https://2020.nwhacks.io/" rel="noopener" target="_blank" class="navbar-item">2020</a>
         </div>
       </div>
     </nav>
